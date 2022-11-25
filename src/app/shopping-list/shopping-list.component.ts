0d00027a53a72ac62508473218a7ec6f250a0c23
abdulmoiez876 <abdul.moiez876@gmail.com>
2022-11-25 13:57:30 +0500

    shopping list dummy data added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Orange', 3),
+    new Ingredient('Banana', 12),
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
